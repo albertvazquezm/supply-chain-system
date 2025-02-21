9ff40fe4248f60c3b82117fc6926f3643dba351a
Albert Vàzquez <hola@albertvazquez.es>
2025-02-21 14:51:17 +0100

    remove unnecessary code

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SupplyChainModule } from './modules/supply-chain/supply-chain.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupplyChainItem } from './db/entities/supply-chain-item.entity';
@@ -21,7 +19,5 @@ import { SupplyChainItemEvent } from './db/entities/supply-chain-item-event.enti
     }),
     SupplyChainModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
